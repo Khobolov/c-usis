dd7f847500a1c89f1ed596576000edd12d145448
Xobolov <farhad.xobolov@gmail.com>
2020-12-24 22:44:21 +0400

    *Some fixes for User & Lesson modules

--- main.c
+++ main.c
@@ -8,21 +8,21 @@ struct Lesson
     int credit;
     int quota;
     int teacher_id;
-
 };
 
-struct Teacher{
+struct Teacher
+{
     int id;
     char name[30];
     char surname[30];
     char title[30];
-
 };
 
-struct Student{
+struct Student
+{
     int id;
-    char name[20];
-    char surname[20];
+    char name[30];
+    char surname[30];
     int lesson_count;
     int credit;
 };
@@ -31,44 +31,43 @@ int main() {
 
     struct Lesson lesson;
     struct Teacher teacher;
+    struct Student student;
 
-    int ch1, ch2, ch3, ch4, ch5;
+    int ch;
 
     menus();
 
-    scanf("%d", &ch1);
+    scanf("%d", &ch);
     printf("\n");
-    switch (ch1)
+    switch (ch)
     {
         case 1:
             menuUser();
-            scanf("%d", &ch2);
+            scanf("%d", &ch);
             printf("\n");
-            switch (ch2)
+            switch (ch)
             {
                 case 1:
                     regTeacher(teacher);
                     break;
-
                 case 2:
-            
-                    break;
-                
+                    regStudent(student);
+                    break;                
             }
             break;
 
         case 2:
             menuUser();
-            scanf("%d", &ch4);
+            scanf("%d", &ch);
             printf("\n");
-            switch (ch4)
+            switch (ch)
             {
             case 1: 
                 //Login Teacher
                 menuLesson();
-                scanf("%d", &ch5);
+                scanf("%d", &ch);
                 printf("\n");
-                switch (ch5)
+                switch (ch)
                 {
                 case 1:
                     addLesson(lesson);
@@ -77,32 +76,32 @@ int main() {
                     editLesson(lesson);
                     break;
                 case 3:
-                    deleteLesson(lesson);
+                    showLesson(lesson);
                     break;
                 case 4: 
-                    showLesson(lesson);
+                    deleteLesson(lesson);
                     break;
                 }
                 break;
             
             case 2:
-                //Login Student
-
+                //Login Student                
                 break;
             }
+
             break;
 
         case 3:
             menuUser();
-            scanf("%d", &ch3);
+            scanf("%d", &ch);
             printf("\n");
-            switch (ch3)
+            switch (ch)
             {
                 case 1:
                     showTeacher(teacher);
                     break;
                 case 2:
-
+                    showStudent(student);
                     break;
              }
             break;
@@ -140,7 +139,7 @@ void regTeacher(struct Teacher teacher)
     int id;
     FILE *fptr;
 
-    fptr = fopen("Users.txt", "a");
+    fptr = fopen("Users/Teachers.txt", "a");
 
         if(fptr == NULL)
         {
@@ -153,11 +152,11 @@ void regTeacher(struct Teacher teacher)
 
         if (availableTeacher(teacher, id) == 1)
         {
-            printf("Id %d is Available in the Users", id);
+            printf("Id %d is Available in the Teachers", id);
         }
         else
         {
-            
+                
             teacher.id = id;
             
             printf("Name: ");
@@ -171,24 +170,79 @@ void regTeacher(struct Teacher teacher)
 
             fwrite (&teacher, sizeof(struct Teacher), 1, fptr);
 
-            if(fwrite != 0)  
-                printf("contents to file written successfully !\n"); 
-            else 
-                printf("error writing file !\n"); 
-
+            if(fwrite != 0) 
+                {
+                    printf("contents to file written successfully !\n"); 
+                } 
+                else 
+                {
+                    printf("error writing file !\n"); 
+                }
+               
             fclose(fptr);
 
             printf("Teacher added\n\n");
         }
+}
 
+void regStudent(struct Student student)
+{
+    int id;
+    FILE *fptr;
 
+    fptr = fopen("Users/Students.txt", "a");
+
+        if(fptr == NULL)
+        {
+            printf("Error!");
+            exit(1);
+        }
+
+            printf("Id: ");
+            scanf("%d", &id);
+
+        if (availableStudent(student, id) == 1)
+        {
+            printf("Id %d is Available in the Students", id);
+        }
+        else
+        {
+            student.id = id;
+            
+            printf("Name: ");
+            scanf("%s", student.name);
+
+            printf("Surname: ");
+            scanf("%s", student.surname);
+
+            printf("Lesson Count: ");
+            scanf("%d", &student.lesson_count);
+
+            printf("Credit: ");
+            scanf("%d", &student.credit);
+
+            fwrite (&student, sizeof(struct Student), 1, fptr);
+
+            if(fwrite != 0) 
+                {
+                    printf("contents to file written successfully !\n"); 
+                } 
+                else 
+                {
+                    printf("error writing file !\n"); 
+                }
+               
+            fclose(fptr);
+
+            printf("Student added\n\n");
+        }
 }
 
 void showTeacher(struct Teacher teacher)
 {
     FILE *fptr;
 
-    fptr = fopen("Users.txt", "r");
+    fptr = fopen("Users/Teachers.txt", "r");
 
     if(fptr == NULL)
     {
@@ -196,13 +250,34 @@ void showTeacher(struct Teacher teacher)
         exit(1);
     }
 
-    while(fread(&teacher, sizeof(struct Lesson), 1, fptr)) 
+    while(fread(&teacher, sizeof(struct Teacher), 1, fptr)) 
     {
-        printf("Id = %d\nName = %s\nSurName = %s\nTitle = %s\n", teacher.id, teacher.name, teacher.surname, teacher.title);
+        printf("Id = %d\nName = %s\nSurName = %s\nTitle = %s\n\n", teacher.id, teacher.name, teacher.surname, teacher.title);
     }
         
     fclose (fptr); 
 }
+
+void showStudent(struct Student student)
+{
+    FILE *fptr;
+
+    fptr = fopen("Users/Students.txt", "r");
+
+    if(fptr == NULL)
+    {
+        printf("Error!");
+        exit(1);
+    }
+
+    while(fread(&student, sizeof(struct Student), 1, fptr)) 
+    {
+        printf("Id = %d\nName = %s\nSurName = %s\nLesson Count = %d\nCredit: %d\n\n", student.id, student.name, student.surname, student.lesson_count, student.credit);
+    }
+        
+    fclose (fptr); 
+}
+
 
 void addLesson(struct Lesson lesson)
 {
@@ -265,7 +340,7 @@ void showLesson(struct Lesson lesson)
 
     while(fread(&lesson, sizeof(struct Lesson), 1, fptr)) 
     {
-        printf ("Code = %d\nName = %s\nCredit = %d\nQuota = %d\n", lesson.code, lesson.name, lesson.credit, lesson.quota);
+        printf ("Code = %d\nName = %s\nCredit = %d\nQuota = %d\n\n", lesson.code, lesson.name, lesson.credit, lesson.quota);
     }
         
     fclose (fptr); 
@@ -278,12 +353,12 @@ void editLesson(struct Lesson lesson)
     FILE *fpo;
     int code, ch;
 
-    printf("Enter Id to update:");
+    printf("Enter Code to update:");
     scanf("%d", &code);
 
-    if (avaibleCode(lesson, code) == 0)
+    if (availableCode(lesson, code) == 0)
     {
-    printf("ID %d is not Available in the file", code);
+        printf("Code %d is not Available in the Lessons", code);
     }
     else
     {
@@ -292,14 +367,14 @@ void editLesson(struct Lesson lesson)
 
         while (fread(&lesson, sizeof(struct Lesson), 1, fpo))
         {
-
         if (lesson.code != code)
             fwrite(&lesson, sizeof(struct Lesson), 1, fpt);
         else
         {
-            printf("\n\t1. Update Title of Lesson %d", lesson.code);
-            printf("\n\t2. Update Subject of Lesson %s", lesson.name);
-            printf("\n\t3. Update both Title and Subject of Lesson %d", lesson.credit);
+            printf("\n\t1. Update Title: %d of Lesson", lesson.code);
+            printf("\n\t2. Update Name: %s of Lesson", lesson.name);
+            printf("\n\t3. Update Credit: %d of Lesson", lesson.credit);
+            printf("\n\t3. Update Quota: %d of Quota", lesson.quota);
             printf("\nEnter your choice:");
             scanf("%d", &ch);
 
@@ -307,7 +382,7 @@ void editLesson(struct Lesson lesson)
             {
             case 1:
                 printf("Code : ");
-                scanf("%s", lesson.code);
+                scanf("%d", &lesson.code);
                 break;
             case 2:
                 printf("Name : ");
@@ -315,10 +390,10 @@ void editLesson(struct Lesson lesson)
                 break;
             case 3:
                 printf("Credit : ");
-                scanf("%d", lesson.credit);
+                scanf("%d", &lesson.credit);
             case 4:
                 printf("Quota : ");
-                scanf("%d", lesson.quota);
+                scanf("%d", &lesson.quota);
                 break;
             default:
                 printf("Invalid Selection");
@@ -348,12 +423,12 @@ void deleteLesson(struct Lesson lesson)
     FILE *fpt;
     int code, s;
 
-    printf("Enter the Id you want to delete :");
+    printf("Enter the Code you want to Delete :");
     scanf("%d", &code);
     
-    if (avaibleCode(lesson, code) == 0)
+    if (availableCode(lesson, code) == 0)
     {
-        printf("Code %d is not available in the file\n", code);
+        printf("Code %d is not Available in the Lessons\n", code);
     }
     else
     {
@@ -376,7 +451,7 @@ void deleteLesson(struct Lesson lesson)
             fwrite(&lesson, sizeof(struct Lesson), 1, fpo);
         }
         
-        printf("\n Lesson deleted!\n");
+        printf("\nLesson deleted!\n");
         fclose(fpo);
         fclose(fpt);
     }
@@ -408,15 +483,13 @@ void deleteLesson(struct Lesson lesson)
 //  Check ID is Available //
 int availableCode(struct Lesson lesson, int code)
 {
-
     FILE *fp;
 
     fp = fopen("Lessons.txt", "r");
     
     while (!feof(fp))
     {
-
-    fread(&lesson, sizeof(struct Lesson), 1, fp);
+        fread(&lesson, sizeof(struct Lesson), 1, fp);
 
         if (code == lesson.code)
         {
@@ -433,7 +506,7 @@ int availableTeacher(struct Teacher teacher, int id)
 {
     FILE *fp;
 
-    fp = fopen("Users.txt", "r");
+    fp = fopen("Users/Teachers.txt", "r");
     
     while (!feof(fp))
     {
@@ -441,6 +514,28 @@ int availableTeacher(struct Teacher teacher, int id)
         fread(&teacher, sizeof(struct Lesson), 1, fp);
 
         if (id == teacher.id)
+        {
+            fclose(fp);
+            return 1;
+        }
+    }
+    fclose(fp);
+
+    return 0;
+}
+
+int availableStudent(struct Student student, int id)
+{
+    FILE *fp;
+
+    fp = fopen("Users/Students.txt", "r");
+    
+    while (!feof(fp))
+    {
+
+        fread(&student, sizeof(struct Lesson), 1, fp);
+
+        if (id == student.id)
         {
             fclose(fp);
             return 1;
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,21 @@
+teacher.id = id;
+            
+            // printf("Name: ");
+            // scanf("%s", teacher.name);
+
+            // printf("SurName: ");
+            // scanf("%s", teacher.surname);
+
+            // printf("Title: ");
+            // scanf("%s", teacher.title);
+
+            // fwrite (&teacher, sizeof(struct Teacher), 1, fptr);
+
+            // if(fwrite != 0)  
+            //     printf("contents to file written successfully !\n"); 
+            // else 
+            //     printf("error writing file !\n"); 
+
+            // fclose(fptr);
+
+            // printf("Teacher added\n\n");
